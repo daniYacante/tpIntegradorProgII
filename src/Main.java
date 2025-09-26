b4496aee3df5c14b5fdc9ce36b0b16ed50e2ada7
daniYacante <danizyacante@gmail.com>
2025-09-26 18:14:47 +0000

    test sql

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Connection conn=null;
+        try{
+            Class.forName("org.sqlite.JDBC");
+            conn=DriverManager.getConnection("jdbc:sqlite:test01:sqlite");
+            if(conn!=null){
+                System.out.println("Conexion a base de datos...OK");
+            }else{
+                System.out.println("Conexion a base de datos...problemas!");
+            }
+            conn.close();
+        }catch(SQLException a){
+            System.out.println(a);
+        }catch(ClassNotFoundException c){
+            System.out.println(c);
+        }catch(Exception e){
+            System.out.println(e);
         }
     }
 }
